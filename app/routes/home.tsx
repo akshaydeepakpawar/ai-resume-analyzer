bcb182898c4034dc2a3ee2f97ec198dc0205aded
akshay pawar <ap3690700@gmail.com>
2025-12-27 18:13:12 +0530

    Authentication done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,10 @@ import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
 import { resume } from "react-dom/server";
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,21 +16,28 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated]);
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
-        <div className="page-heading">
-          <h1 className="">Track Your Applications & Resume Rating</h1>
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Rating</h1>
           <h2>Review your submission and receive AI-powered feedback.</h2>
         </div>
-      {resumes.length > 0 && (
-        <div className="resume-section">
-          {resumes.map((resume) => (
-            <ResumeCard key={resume.id} resume={resume} />
-          ))}
-        </div>
-      )}
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
       </section>
     </main>
   );
